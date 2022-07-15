50e3ee69b1abc82715342e43cadfe8e9e1bab569
samyhaff <haffoudhisamy@gmail.com>
2022-07-15 11:22:16 +0200

    added redirect

---
+++ p4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failded\n");
+        exit(1);
+    } else if (rc == 0) {
+        close(STDOUT_FILENO);
+        open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+
+        char *myargs[3];
+        myargs[0] = strdup("wc");
+        myargs[1] = strdup("p3.c");
+        myargs[2] = NULL;
+        execvp(myargs[0], myargs);
+        printf("Not printed\n");
+    } else {
+        int rc_wait = wait(NULL);
+    }
+    return 0;
+}
